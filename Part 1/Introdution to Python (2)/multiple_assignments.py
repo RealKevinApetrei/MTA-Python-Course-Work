fb5b2ceb890785ad045cb94fd5f876ba7fb18b47
Kevin Apetrei <realkevinapetrei@gmail.com>
2020-08-10 23:49:25 +0100

    Update

--- Part 1/Introdution to Python (2)/multiple_assignments.py
+++ Part 1/Introdution to Python (2)/multiple_assignments.py
@@ -1,5 +1,5 @@
-justin_job = "Edutainer"
-vonne_job = "Edutainer"
+aubri_job = wes_job = justin_job = vonne_job = "Edutainer"
+# vonne_job = "Edutainer"
 
 print(f"Justin Job: {justin_job}")
 print(f"Vonne Job: {vonne_job}")
