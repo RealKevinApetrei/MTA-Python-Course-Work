8341caa824926ab7fd0c6dbc5da40414271c647b
Kevin Apetrei <realkevinapetrei@gmail.com>
2020-08-10 23:45:09 +0100

    Update

--- Part 1/Introdution to Python (2)/data_types.py
+++ Part 1/Introdution to Python (2)/data_types.py
@@ -7,7 +7,18 @@ print(likes_cheese)
 print("Has 13 Apples")
 print(has13Apples)
 
+# These are string variables
+
 favorite_color = "blue"
 other_color = "Blue suede shoes"
 print(favorite_color)
 print(other_color)
+
+# This is integer variables
+favorite_number = 13
+
+# This is a float variable
+tax_rate = 1.07
+
+print(favorite_number)
+print(tax_rate)
---
+++ Part 1/Introdution to Python (2)/multiple_assignments.py
@@ -0,0 +1,5 @@
+justin_job = "Edutainer"
+vonne_job = "Edutainer"
+
+print(f"Justin Job: {justin_job}")
+print(f"Vonne Job: {vonne_job}")
