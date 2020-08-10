43b1435b28dda0ca0fe2ac0549cac4b55a9cc006
Kevin Apetrei <realkevinapetrei@gmail.com>
2020-08-11 00:11:15 +0100

    Update

--- Part 1/Introdution to Python (2)/multiple_assignments.py
+++ Part 1/Introdution to Python (2)/multiple_assignments.py
@@ -1,5 +1,18 @@
+# Multiple variables assigned to the same value
 aubri_job = wes_job = justin_job = vonne_job = "Edutainer"
 # vonne_job = "Edutainer"
 
 print(f"Justin Job: {justin_job}")
 print(f"Vonne Job: {vonne_job}")
+print(f"Aubri Job: {aubri_job}")
+print(f"Wes Job: {wes_job}")
+
+hair_color, eye_color, shirt_color, age = "red", "blue", "black", 23
+# eye_color = "blue"
+# shirt_color = "black"
+# age = 23
+
+print(f"Justin's Hair Color: {hair_color}")
+print(f"Justin's Eye Color: {eye_color}")
+print(f"Justin's Shirt Color: {shirt_color}")
+print(f"Justin's Age: {age}")
