26188ded0915fd8b443cb50dc63c8b5ef3fe947f
Kevin Apetrei <realkevinapetrei@gmail.com>
2020-08-11 15:55:30 +0100

    Update

---
+++ Part 1/Introdution to Python (2)/modifying_lists.py
@@ -0,0 +1,28 @@
+edutainers = ["Adam", "Aubri", "Daniel", "Jo"]
+
+print(f"Edutainers: {edutainers}")
+
+# How do I add an element to the end of a list?
+
+edutainers.append("Justin")
+
+print(f"Edutainers: {edutainers}")
+
+# How do I remove an element from a list?
+
+del edutainers[3]
+
+print(f"Edutainers: {edutainers}")
+
+# How do I remove an element from the end of a list?
+
+special = edutainers.pop()
+
+print(f"Edutainers: {edutainers}")
+print(f"Special: {special}")
+
+# How do I change an element in a list?
+
+ edutainers[2] = "Dan"
+
+print(f"Edutainers: {edutainers}")
