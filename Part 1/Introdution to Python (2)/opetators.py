37e0c5578d8bea1ce47ae121abe8124ac420aba0
Kevin Apetrei <realkevinapetrei@gmail.com>
2020-08-11 17:12:34 +0100

    Update

---
+++ Part 1/Introdution to Python (2)/opetators.py
@@ -0,0 +1,61 @@
+# Mathematical Operations
+
+# + = Add
+# - = Subtract
+# / = Divide
+# * = Multiply
+# % = Modulus (MOD) (Remainder)
+# ** = Power
+# // = Floor Division
+
+# Add
+
+print(f"3 + 1 is: {3 + 1}")
+
+# Subtract
+
+print(f"3 - 1 is: {3 - 1}")
+
+# Divide and Floor Division
+
+print(f"6 / 2 is: {6 / 2}")
+print(f"7 / 2 is: {7 / 2}")
+
+print(f"7 // 2 is: {7 // 2}")
+print(f"7.3 // 2.478 is: {7.3 // 2.478}")
+
+# Modulus
+
+print(f"7 % 2 is: {7 % 2}")
+print(f"6 % 2 is: {6 % 2}")
+
+# Multiply and Power
+
+print(f"7 * 2 is: {7 * 2}")
+
+print(f"7 ** 2 is: {7 ** 2}")
+print(f"4 ** 5 is: {4 ** 5}")
+
+# What about other types? (String)
+
+print("a" + "b") # Concatenation
+print("Vonne" * 3)
+
+# Comparison
+
+# < = Strictly less to
+# <= = Less than or equal to
+# > = Strictly larger to
+# >= = Larger than or equal to
+# == = Strictly equal to
+
+print(3 < 4)
+print(3 <= 4)
+print(3 > 4)
+print(3 >= 4)
+print(3 == 4)
+
+word_one = "cheese"
+word_two = "1"
+
+print(f"Is cheese < 1: {word_one < word_two}")
