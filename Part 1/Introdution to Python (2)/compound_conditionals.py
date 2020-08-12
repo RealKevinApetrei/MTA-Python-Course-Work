70a7dbdb75ee201028c982277b0b4b8b4bd19dbe
Kevin Apetrei <realkevinapetrei@gmail.com>
2020-08-12 19:14:19 +0100

    Update

---
+++ Part 1/Introdution to Python (2)/compound_conditionals.py
@@ -0,0 +1,22 @@
+# eaten_peas = False
+# eaten_carrots = True
+
+# if eaten_peas or eaten_carrots:
+    # print("You get Ice Cream!")
+
+# else:
+    # print("You get nothing!")
+
+# /
+
+give_me_money = False
+fuel_my_car = True
+
+if give_me_money and fuel_my_car:
+    print("No... You are too generous.")
+
+elif give_me_money or fuel_my_car:
+    print("Thank You!")
+
+else:
+    print("I need your help.")
