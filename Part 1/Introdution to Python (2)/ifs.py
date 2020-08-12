12b101752695a57ac2790d7848b16514a456a8c7
Kevin Apetrei <realkevinapetrei@gmail.com>
2020-08-12 18:46:02 +0100

    Update

---
+++ Part 1/Introdution to Python (2)/ifs.py
@@ -0,0 +1,23 @@
+has_eaten_brussel_sprouts = False
+
+if has_eaten_brussel_sprouts:
+    print("Yay!!! Popsicle!")
+
+# else:
+print("Here be your coal...")
+
+# Gameish?
+
+guess = 7
+
+if guess == 6:
+    print("You win!")
+
+elif guess == 7:
+    print("Runner up!")
+
+elif guess == 5:
+    print("Runner up!")
+
+else:
+    print("Uh oh")
