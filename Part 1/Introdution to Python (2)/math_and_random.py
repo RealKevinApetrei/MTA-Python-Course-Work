f5c608bd33c5077de9fa705f77b3a3ad2428ca21
Kevin Apetrei <realkevinapetrei@gmail.com>
2020-08-14 23:20:01 +0100

    Update

---
+++ Part 1/Introdution to Python (2)/math_and_random.py
@@ -0,0 +1,11 @@
+# import math
+#
+# result = math.ceil(4.3)
+# print(result)
+#
+# result = math.fsum([1, 2, 3.483, 384930.0234930958430945689403, 2349])
+# print(result)
+
+import random
+
+secret_number = random.randrange(1, 102, 2)
