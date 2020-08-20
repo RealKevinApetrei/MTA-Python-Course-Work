88e34d2959fbe0f4cedf0d624215021361cec53b
Kevin Apetrei <realkevinapetrei@gmail.com>
2020-08-20 21:39:54 +0100

    Update

---
+++ Part 1/Python Programming (4)/standard_lib_functions.py
@@ -0,0 +1,30 @@
+import math as m
+from os import walk, mkdir, chdir, listdir, getcwd, rmdir
+
+
+if __name__ == "__main__":
+    # my_list = [1, 2, 3, 4]
+    # str_list = ["one", "two", "three"]
+    # str_dict = {
+    #     "a": "apple",
+    #     "b": "baseball",
+    #     "c": "chicken"
+    # }
+    #
+    # my_tuple = (1, 2, 3, 4, 5)
+    #
+    # print(m.ceil(2.5))
+    # print(m.floor(2.5))
+    # print(m.sqrt(4))
+
+    # for item in walk("/"):
+    #     print(item)
+
+    # mkdir("test_folder")
+    # chdir("./test")
+    # rmdir("test")
+    # print(listdir("./"))
+    # print(getcwd())
+
+    # for thing in listdir("./test"):
+    #     rmdir(thing)
