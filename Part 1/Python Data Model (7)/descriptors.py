2d4835ad44e13e4db65a74ffca353c4f0a07496a
Kevin Apetrei <realkevinapetrei@gmail.com>
2020-09-03 15:01:13 +0100

    Update

--- Part 1/Python Data Model (7)/descriptors.py
+++ Part 1/Python Data Model (7)/descriptors.py
@@ -1,6 +1,28 @@
+import re
+
+class VIN:
+    _validation_regex = re.compile(r"\d{9}")
+    def __init__(self, vin="000000000"):
+        self.vin = vin
+
+    def __get__(self, instance, owner):
+        return self._vin
+
+    def __set__(self, instance, new_value): #*args, **kwargs
+        if not self._validation_regex.match(new_value):
+            print("Your 'vin' must adhere to the appropriate formatting.")
+            return
+        self._vin = new_value
+
+    def __delete__(self, instance):
+        print("No! You can't delete 'vim' number.")
+
 class Car:
+    vin = VIN()
     def __init__(self, color):
         self._color = color
+        # if not vin is None:
+        #     self.vin = VIN(vin)
 
     @property
     def color(self):
